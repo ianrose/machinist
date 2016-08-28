d4635034e36544f6120749510f56e4d6abf682bc
Ian Rose <ian@ianrose.me>
2016-08-28 08:08:14 -0400

    dev vs production

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Set a ture or false for production/development. Use to run certain plugins
+var devBuild = ((process.env.NODE_ENV || '').trim().toLowerCase() !== 'production');
+
 var fs = require('fs');
 var path = require('path');
 var Metalsmith = require('metalsmith');
@@ -6,13 +9,13 @@ var layouts = require('metalsmith-layouts');
 var assets = require('metalsmith-assets');
 var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
-var browserSync = require('metalsmith-browser-sync');
+var browserSync = devBuild ? require('metalsmith-browser-sync') : null;
 var metadata = require('metalsmith-metadata');
 var sass = require('metalsmith-sass');
 var inplace = require('metalsmith-in-place');
 var debug = require('metalsmith-debug');
 var postcss = require('metalsmith-with-postcss');
-
+var pkg = require('./package.json');
 
 var dataFiles = fs.readdirSync(path.join(__dirname, 'src', 'data'));
 var data = {};
@@ -21,7 +24,7 @@ dataFiles.forEach(function (filename) {
   data[filename.split('.')[0]] = 'data/' + filename;
 });
 
-Metalsmith(__dirname)
+var ms = Metalsmith(__dirname)
   .source('src/')
   .destination('./build/')
   .use(metadata(data))
@@ -63,18 +66,21 @@ Metalsmith(__dirname)
   .use(assets({
     source: './src/assets', // relative to the working directory
     destination: './assets' // relative to the build directory
-  }))
-  .use(browserSync({
+  }));
+
+  if(devBuild) ms.use(browserSync({
     server: './build',
     files: ['src/**/*.*', 'layouts/*.*', 'partials/**/*.*'],
     open: false,
     notify: false
-  }))
-  .use(debug({
+  }));
+
+  ms.use(debug({
     files: false,
     match: "**/*.md"
   }))
   .build(function (error) {
+    console.log((devBuild ? 'Development' : 'Production'), 'build success, version', pkg.version);
     if (error) {
       console.log(error);
     }
